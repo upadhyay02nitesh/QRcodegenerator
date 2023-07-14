a143c92a03c84f4f9f1fce8832660d4c47d664d0
Nitesh upadhyay <92724753+upadhyay02nitesh@users.noreply.github.com>
2023-07-14 21:07:07 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -6,7 +6,7 @@ from pyqrcode import QRCode
 
 
 # String which represents the QR code
-s = "https://www.linkedin.com/in/nitesh02upadhyaya/"
+s = "https://www.linkedin.com/in/nitesh02upadhyayaa/"
 
 # Generate QR code
 url = pyqrcode.create(s)
